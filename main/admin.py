6ec2d8fe4a14b5e9356f2a2218890871e9a61d94
Murilo Santori Boff <murilosantori45@gmail.com>
2024-01-30 06:35:37 -0300

    Database Creation and putting some data

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Clientes)
+admin.site.register(models.Produtos)
+admin.site.register(models.Pedidos)
+admin.site.register(models.Feedback)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Clientes(models.Model):
+    nome = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=500)
+    telefone = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+class Produtos(models.Model):
+    nome = models.CharField(max_length=200)
+    preço = models.FloatField()
+    descricao = models.TextField()
+    estoque = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+class Pedidos(models.Model):
+    cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+    produtos = models.ManyToManyField(Produtos)
+    quantidade = models.IntegerField(default=0)
+    data_pedido = models.DateField()
+    data_entrega = models.DateField()
+    valor_total = models.FloatField()
+
+    def __str__(self):
+        return f'Pedido {self.id}'
+
+class Feedback(models.Model):
+    choices = (
+        ('B', 'Bom'),
+        ('R', 'Ruim')
+    )
+    escolha = models.CharField(max_length=1, choices=choices)
+    feedback = models.TextField()
+
+    def __str__(self):
+        return f'Feedback {self.id}'
