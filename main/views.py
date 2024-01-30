41265f61b298518654f0e4690bf40cf298428888
Murilo Santori Boff <murilosantori45@gmail.com>
2024-01-30 20:51:25 -0300

    Starting the home page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 # Usar o Login Required
+# Fazer a função de Logout
+
+def logout_sair(request):
+    logout(request)
+    return redirect('/users/login/')
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
